01e955af4fd9d4636e6960d470f8d10618cd44fb
Himanshu Kumar Gupta <himanshu.gupta572554@gmail.com>
2026-01-20 20:33:06 +0530

    Day 3

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 export default function TextForm(props) {
   const [text, setText] = useState("Enter text here" );
   const handleUpclick =()=> {
-    console.log("Uppercase was clicked" + text )
+    // console.log("Uppercase was clicked" + text )
     let newtext = text.toUpperCase();
     setText(newtext)
   }
   const handleOnChange =(event)=>{
-    console.log("On Change");
+    // console.log("On Change");
     setText(event.target.value)
      }
   return (
